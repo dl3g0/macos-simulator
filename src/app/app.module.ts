8ff5047457365b4cd86b4dc08f0fa21af818b58d
unknown <diegojf123@gmail.com>
2024-06-06 20:14:19 -0500

    feature: add translate , add themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './modules/primeng/primeng.module';
 import { GlobalModule } from './modules/global/global.module';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
+export function HttpLoaderFactory( http: HttpClient ){
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +21,14 @@ import { GlobalModule } from './modules/global/global.module';
     AppRoutingModule,
     GlobalModule,
     BrowserAnimationsModule,
-    PrimengModule
+    PrimengModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,18 +1,38 @@
 import { Inject, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core'
 import { PrimeNGConfig } from 'primeng/api';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
+  public lang: string;
+  public theme:string;
   request = new BehaviorSubject<Object>(undefined);
   request$ = this.request.asObservable();
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private translateService: TranslateService,
+    private config: PrimeNGConfig,
+  ) { }
 
   sendRequest(data){
     this.request.next(data);
+  }
+
+  switchTheme(theme: string) {
+    this.theme = theme;
+    let themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
+    if (themeLink) {
+      themeLink.href = theme + '.css';
+    }
+  }
+
+  changeLanguage(type: string) {
+    this.lang = type;
+    this.translateService.use(type);
+    this.translateService.get('primeng').subscribe((res) => this.config.setTranslation(res));
   }
 }
